568b786c0bae3a64f41d350d83c5b27c9f09ccf8
Victor Spirin <greatgomer2@gmail.com>
2020-09-08 22:36:24 +0300

    Navigation bar is ready

--- app/src/main/java/com/example/freeroller/MainActivity.kt
+++ app/src/main/java/com/example/freeroller/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.freeroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragment1: Fragment =
+        MapsFragment()
+    private val fragment2: Fragment = TrainsFragment()
+    private val fm: FragmentManager = supportFragmentManager
+    private var active = fragment1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigation: BottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit()
+        fm.beginTransaction().add(R.id.main_container, fragment1, "1").commit()
     }
+
+    private val mOnNavigationItemSelectedListener: BottomNavigationView.OnNavigationItemSelectedListener =
+        object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(@NonNull item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        fm.beginTransaction().hide(active).show(fragment1).commit()
+                        active = fragment1
+                        return true
+                    }
+                    R.id.navigation_dashboard -> {
+                        fm.beginTransaction().hide(active).show(fragment2).commit()
+                        active = fragment2
+                        return true
+                    }
+                }
+                return false
+            }
+        }
 
 }
